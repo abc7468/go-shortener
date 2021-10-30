ce37dcf8cd71446ee170454779d6565177dd0e84
abc7468 <abc7468@naver.com>
2021-10-31 00:45:28 +0900

    feat : URL CRUD 기능 생성

--- DB/DB.go
+++ DB/DB.go
@@ -13,7 +13,7 @@ var db *bolt.DB
 
 func InitDB() {
 	if db == nil {
-		dbPointer, err := bolt.Open("myDb.db", 0600, nil)
+		dbPointer, err := bolt.Open("db.db", 0600, nil)
 		db = dbPointer
 		if err != nil {
 			panic(err)
@@ -36,7 +36,7 @@ func Close() {
 
 }
 
-func SaveURL(original, shortener string) {
+func SaveURL(shortener, original string) {
 	err := db.Update(func(t *bolt.Tx) error {
 		bucket := t.Bucket([]byte(dataBucket))
 		err := bucket.Put([]byte(shortener), []byte(original))
@@ -44,5 +44,43 @@ func SaveURL(original, shortener string) {
 	})
 	if err != nil {
 		panic(err)
+
 	}
+}
+func LoadUrl(shortener string) []byte {
+	var data []byte
+	db.View(func(t *bolt.Tx) error {
+		bucket := t.Bucket([]byte(dataBucket))
+		data = bucket.Get([]byte(shortener))
+		return nil
+	})
+	return data
+}
+func LoadAllUrls() [][]string {
+	var urls [][]string
+
+	db.View(func(tx *bolt.Tx) error {
+		// Assume bucket exists and has keys
+		b := tx.Bucket([]byte(dataBucket))
+
+		b.ForEach(func(k, v []byte) error {
+			val := make([]string, 2)
+			val[0], val[1] = string(k), string(v)
+			urls = append(urls, val)
+			return nil
+		})
+		return nil
+	})
+	return urls
+}
+func DeleteUrl(shortener string) error {
+	err := db.Update(func(t *bolt.Tx) error {
+		bucket := t.Bucket([]byte(dataBucket))
+		err := bucket.Delete([]byte(shortener))
+		return err
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
